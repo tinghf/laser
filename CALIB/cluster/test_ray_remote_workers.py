a075cd7ab545d599d3208a28243613637aacf46e
Tony Ting <tinghf@gmail.com>
2025-03-11 10:50:53 -0700

    add a small ray optuna test script using postgresql as backend

---
+++ CALIB/cluster/test_ray_remote_workers.py
@@ -0,0 +1,43 @@
+import optuna
+import ray
+import os
+from model import objective  # Import the objective function
+
+
+# A Ray remote function to execute one trial of the study
+@ray.remote
+def evaluate_trial(study_name, storage_url, trial_number):
+    # Connect to the existing study
+    study = optuna.load_study(study_name=study_name, storage=storage_url)
+    trial = study.ask()
+    value = objective(trial)
+    study.tell(trial, value)
+    return value, trial.params
+
+# Main function to set up study and distribute trials
+def main(num_trials):
+    ray.init()  # Initialize Ray
+
+    # Set up Optuna study with a central storage in SQLite
+    storage = "postgresql://{}:{}@postgres:5432/{}".format(
+        os.environ["POSTGRES_USER"],
+        os.environ["POSTGRES_PASSWORD"],
+        # os.environ["POSTGRES_DB"],)  ToDo:  this should be defined as env in the pod spec, it's missing now so we just hardcode it for the timebeing
+        "optunaDatabase",)
+    study_name = "distributed_optuna"
+    study = optuna.create_study(study_name=study_name, storage=storage, direction="minimize", load_if_exists=True)
+
+    # List to hold remote function references
+    futures = []
+    for _ in range(num_trials):
+        futures.append(evaluate_trial.remote(study_name, storage, _))
+
+    # Gather all results
+    results = ray.get(futures)
+    best_result = min(results, key=lambda x: x[0])
+
+    print("Best result: ", best_result)
+    ray.shutdown()
+
+if __name__ == "__main__":
+    main(100)  # Running 100 trials
