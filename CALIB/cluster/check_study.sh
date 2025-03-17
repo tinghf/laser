7bbab8cfe591e840c0e487eb670dc32cfbb1dfc0
Tony Ting <tinghf@gmail.com>
2025-03-17 16:26:42 -0700

    parameterize study name

--- CALIB/cluster/check_study.sh
+++ CALIB/cluster/check_study.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-optuna studies --storage "mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@mysql:3306/${MYSQL_DB}" | grep spatial_demo_calibration_on_aks4 > /dev/null
+STUDY_NAME=$1
+
+optuna studies --storage "mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@mysql:3306/${MYSQL_DB}" | grep "${STUDY_NAME}" > /dev/null
 echo $?
